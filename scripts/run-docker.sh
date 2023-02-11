94bb6afcf286d197d10afaa34d4a58531442f3f9
Sheng Fan <fredtools999@gmail.com>
2023-02-11 23:11:37 +0800

    see the prev one

--- scripts/run-docker.sh
+++
@@ -1,1 +0,0 @@
-docker run --rm -it --network=host ubuntu:latest bash -c "apt update && apt install curl git -y && bash -c \"curl https://raw.githubusercontent.com/fred913/ChatGPT-Proxy/main/scripts/run-linux-3.sh | sh\""
