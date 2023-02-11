e29f9be31cb633f34914213bcb90bcfdbfb1d064
Sheng Fan <fredtools999@gmail.com>
2023-02-11 20:44:49 +0800

    fix docker network accessibility

--- scripts/run-docker.sh
+++ scripts/run-docker.sh
@@ -1,1 +1,1 @@
-docker run --rm -it ubuntu:latest bash -c "apt update && apt install curl git -y && bash -c \"curl https://raw.githubusercontent.com/fred913/ChatGPT-Proxy/main/scripts/run-linux-2.sh | sh\""
+docker run --rm -it --network=host ubuntu:latest bash -c "apt update && apt install curl git -y && bash -c \"curl https://raw.githubusercontent.com/fred913/ChatGPT-Proxy/main/scripts/run-linux-2.sh | sh\""
