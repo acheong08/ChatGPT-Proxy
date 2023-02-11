11af22af56f121a5dc7206151ca800ae3078b5ec
Sheng Fan <fredtools999@gmail.com>
2023-02-11 20:39:23 +0800

    fix another bug

--- scripts/run-docker.sh
+++ scripts/run-docker.sh
@@ -1,1 +1,1 @@
-docker run --rm -it ubuntu:latest bash -c "apt update && apt install curl -y && curl https://raw.githubusercontent.com/fred913/ChatGPT-Proxy/main/scripts/run-linux.sh | sh"
+docker run --rm -it ubuntu:latest bash -c "apt update && apt install curl git -y && bash -c\"curl https://raw.githubusercontent.com/fred913/ChatGPT-Proxy/main/scripts/run-linux.sh | sh\""
