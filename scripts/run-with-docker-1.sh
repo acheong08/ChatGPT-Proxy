56124f73a4d64159f8f83f902d96ed89c6fd5df7
devmitrandir <59085306+devmitrandir@users.noreply.github.com>
2023-02-13 14:49:21 +0300

    Update docker (#18)

--- scripts/run-with-docker-1.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if ! [ -e x.txt ] then
-curl -O https://raw.githubusercontent.com/acheong08/ChatGPT-Proxy/main/config.example.json
-echo "Config file generated! Please edit it and re-run this script."
-exit 1
-fi
-docker run -it --rm -v $(pwd)/config.json:/app/config.json:ro --network=host fred913/chatgpt_proxy:latest
